73961c72810c485953ad617a81b7872ed5cdb04c
gReen7aRRow <f.gusarov@inbox.ru>
2022-11-02 02:12:59 +0300

    update labels tests

--- task_manager/apps/labels/tests.py
+++ task_manager/apps/labels/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse_lazy
 from task_manager.apps.users.models import TaskUser
 from task_manager.apps.labels.models import Label
 from task_manager.apps.statuses.models import Status
@@ -11,12 +12,29 @@ class LabelsTest(TestCase):
             password='Zde6v45rGBYx2LGx',
         )
         self.client.force_login(self.user)
-        Label.objects.create(name='Test')
+        self.label1 = Label.objects.create(name='first')
+        self.label2 = Label.objects.create(name='second')
+        self.label3 = Label.objects.create(name='third')
 
     def test_create(self):
         response = self.client.post('/labels/create/', {'name': 'Test1'})
         self.assertEqual(response.status_code, 302)
         self.assertTrue(Label.objects.filter(name="Test1"))
+
+    def test_labels_list(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('labels'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            template_name='labels/labels.html',
+        )
+
+        tasks_list = list(response.context['object_list'])
+        self.assertQuerysetEqual(
+            tasks_list,
+            [self.label1, self.label2, self.label3],
+        )
 
     def test_update(self):
         response = self.client.post('/labels/1/update/', {'name': 'Test2'})
@@ -25,6 +43,8 @@ class LabelsTest(TestCase):
 
     def test_delete(self):
         response = self.client.post('/labels/1/delete/')
+        response = self.client.post('/labels/2/delete/')
+        response = self.client.post('/labels/3/delete/')
         self.assertEqual(response.status_code, 302)
         self.assertEqual(Label.objects.count(), 0)
 
