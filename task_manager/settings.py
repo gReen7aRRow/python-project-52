525b6a99f4f6750fbdd742fee1777a29bbf8a205
gReen7aRRow <f.gusarov@inbox.ru>
2022-11-03 17:29:14 +0300

    cleaning linter

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-#from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from django.urls import reverse_lazy
 import rollbar  # type: ignore
 import os
@@ -20,7 +20,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#load_dotenv(os.path.join(BASE_DIR, ".env"))
+# load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
